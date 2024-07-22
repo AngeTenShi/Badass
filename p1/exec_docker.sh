4a2cf1a48196492a8976928e59911fdb942c72b0
AngeTenShi <megazombie06pro@gmail.com>
2024-07-22 16:11:02 +0200

    better script p1

--- p1/exec_docker.sh
+++ p1/exec_docker.sh
@@ -1,6 +1,2 @@
-mv router.Dockerfile Dockerfile
-docker build -t frr .
-mv Dockerfile router.Dockerfile
-mv achansel.Dockerfile Dockerfile
-docker build -t achansel_host .
-mv Dockerfile achansel.Dockerfile
+docker build -t frr -f router.Dockerfile .
+docker build -t achansel_host -f achansel.Dockerfile .
